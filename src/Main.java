192eba64830387778634fd27b3a3e716778b5b0a
Валентина Андреева <andre8159@gmail.com>
2023-08-16 09:45:03 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int initialBalans = 100;
+        int addend = 300;
+        int bonus = 0;
+        bonus = addend / 100;
+
+        int finalBalans = initialBalans + addend + bonus;
+        System.out.println("Итоговый баланс: " + initialBalans);
+
+        if (addend > 1000) {
+            bonus = addend / 100;
+
+        } else {
+            System.out.println("Итоговый бонус: " + bonus);
+        }
+
+    }
+}
